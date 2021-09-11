1d06c2f70d647b99b07dfc300170213b80836084
Ronan-O-Murchu <ronan.murphy.design@gmail.com>
2021-09-11 06:58:25 +0000

    DOCS: create validation function for main menu user selections

--- run.py
+++ run.py
@@ -14,6 +14,38 @@ SHEET = GSPREAD_CLIENT.open('rental_listings')
 
 rentals = SHEET.worksheet('rentals')
 
-data = rentals.get_all_values()
 
-print(data)
+def load_start():
+
+    """
+    This code displays the main menu and requires the
+    user to make a selection in order to continue.
+    """
+
+    print("Welcome, please make a choice from the options below.")
+    print("Input 1, 2 or 3 and select enter to make a selection.""\n")
+    print("1: View all listings")
+    print("2: Add a listing")
+    print("3: Delete a listing""\n")
+
+
+def validate_selection():
+
+    """
+    This code validates the users selection on the main menu
+    """
+
+    user_choice = int(input("Make a selection between 1 and 3: "))
+    while user_choice in [1, 2, 3]:
+        print("Thank you, that was a valid selection")
+        break
+    else:
+        print("That is not a valid selection,")
+        print("please enter a selection between 1 and 3.")
+        validate_selection()
+
+    # def view_all_listings():
+    #     view_all = rentals.get_all_values()
+
+load_start()
+validate_selection()
