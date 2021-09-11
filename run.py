9fe8684a8214cc5fa45cfd82db4f01474743e5d9
Ronan-O-Murchu <ronan.murphy.design@gmail.com>
2021-09-11 07:48:49 +0000

    DOCS: include placeholder info for add listing function and tidy up code from PEP8 validation

--- run.py
+++ run.py
@@ -23,10 +23,10 @@ def load_start():
     """
 
     print("Welcome, please make a choice from the options below.")
-    print("Input 1, 2 or 3 and select enter to make a selection.""\n")
+    print("Input 1, 2 or 3 and select enter to make a selection.\n")
     print("1: View all listings")
     print("2: Add a listing")
-    print("3: Delete a listing""\n")
+    print("3: Delete a listing\n")
 
 
 def validate_selection():
@@ -36,16 +36,53 @@ def validate_selection():
     """
 
     user_choice = int(input("Make a selection between 1 and 3: "))
-    while user_choice in [1, 2, 3]:
-        print("Thank you, that was a valid selection")
-        break
+    if user_choice == 1:
+        display_listings()
+    elif user_choice == 2:
+        add_listings()
+    elif user_choice == 3:
+        add_listings()
     else:
         print("That is not a valid selection,")
         print("please enter a selection between 1 and 3.")
         validate_selection()
 
-    # def view_all_listings():
-    #     view_all = rentals.get_all_values()
+
+def display_listings():
+
+    """
+    This code will display the listings when the user makes
+    the selection "1" from the main menu.
+    """
+
+    display_listings = rentals.get_all_values()
+    print(display_listings)
+
+
+def add_listings():
+
+    """
+    This code will allow the user to add a listing to the list.
+    """
+
+    """
+    Plan here is to create code to load the info from the
+    top row and have the user input data.
+    1. The user will need to input data for each section.
+    2. The data will need to be validated.
+    3. The data will all need to be stored and displayed together
+    before submission.
+    4. The user will need to confirm the info to add to the database.
+    """
+
+    print("Please make sure info is correct")
+    ref = int(input("Please enter the reference number: "))
+    if len(ref) != 8:
+        print("That is not a valid input, please try again.")
+        print("Example: AKL-1234")
+    else:
+        print("That is a valid entry, thank you")
+
 
 load_start()
 validate_selection()
