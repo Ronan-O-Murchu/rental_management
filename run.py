8489505b1f05a3117fe27d7418617ee23ac3d0ea
Ronan-O-Murchu <ronan.murphy.design@gmail.com>
2021-09-15 08:51:43 +0000

    DOCS: update add listing function to send the correct data to the database

--- run.py
+++ run.py
@@ -22,11 +22,15 @@ def load_start():
     user to make a selection in order to continue.
     """
 
-    print("\nWelcome, please make a choice from the options below.\n")
+    print("\nWelcome to the online property management app for the")
+    print("Auckland, Wellington and Christchurch regions.\n")
+
+    print("Please make a choice from the options below.\n")
+
     print("Type 1, 2 or 3 and press enter to make the selection.\n")
-    print("1: View all listings")
-    print("2: Add a listing")
-    print("3: Delete a listing\n")
+    print("1: View most recent listings")
+    print("2: Add a property to the database")
+    print("3: Remove a property from the database\n")
 
 
 def validate_selection():
@@ -50,14 +54,14 @@ def validate_selection():
 def display_latest_listings():
 
     """
-    This code will display the listings when the user makes
+    This code will display the 20 most recent listings when the user makes
     the selection "1" from the main menu.
     """
 
     rows_data = []
     global show_listings
     show_listings = rentals.get_all_values()
-    for rows_data in range(1, 11):
+    for rows_data in range(1, 21):
         row = rentals.row_values(rows_data)
         print(*row)
     load_start()
@@ -67,18 +71,15 @@ def display_latest_listings():
 def add_reference():
 
     """
-    This code will allow the user to add a listing to the list
+    This code will allow the user to add a property to the database
     when the user selects "2" from the main menu.
-    """
 
-    """
-    Plan here is to create code to load the info from the
-    top row and have the user input data.
-    1. The user will need to input data for each section.
-    2. The data will need to be validated.
-    3. The data will all need to be stored and displayed together
-    before submission.
-    4. The user will need to confirm the info to add to the database.
+    The user is will be prompted with entry requests for each value
+    input and the function will validate each users input. Each function
+    will display a request of the type of input it requries and will
+    print a message to indicate if it was valid or not. Once the user
+    provides valid data the functions will keep moving down the list
+    until the data is uploaded to the database.
     """
 
     user_reference = []
@@ -94,13 +95,11 @@ def add_reference():
         add_location()
 
 
-def check_value(the_value, the_array):
-    if the_value in the_array:
-        return True
-    else:
-        return False
-
 def add_location():
+
+    """
+    This function requests the property location from the user.
+    """
 
     user_location = []
     global input_location
@@ -116,18 +115,12 @@ def add_location():
         add_location()
 
 
-# def main_func():
-#     reference = add_reference()
-#     add_location(reference)
-
-#     def add_location():
-#         return input_location
-
-#     def add_location(result):
-#         print(result)
-
-
 def add_bedrooms():
+
+    """
+    This function requests how many bedrooms the property has from the user.
+    The users input is also converted into an INT.
+    """
 
     user_bedrooms = []
     global input_bedrooms
@@ -145,6 +138,10 @@ def add_bedrooms():
 
 def add_parking():
 
+    """
+    This function requests if the property has parking available from the user.
+    """
+
     user_parking = []
     global input_parking
     input_parking = input("Please enter if parking is available: ")
@@ -161,10 +158,14 @@ def add_parking():
 
 def add_cost():
 
+    """
+    This function requests the property cost from the user.
+    """
+
     user_cost = []
     global input_cost
     input_cost = input("Please enter the cost: ")
-    price = ["300", "350", "400", "450", "500", "550", "600"]
+    cost = ["300", "350", "400", "450", "500", "550", "600"]
 
     if check_value(input_cost, cost):
         print("That is a valid entry, thank you\n")
@@ -176,6 +177,10 @@ def add_cost():
 
 
 def add_type():
+
+    """
+    This function requests the property type from the user.
+    """
 
     user_type = []
     global input_type
@@ -192,25 +197,39 @@ def add_type():
 
 
 def makeCap(the_word):
+
+    """
+    This function used in conjuction with the add_data functions
+    above makes the users inputs the correct format to update the database.
+    It will capitalise the first letter of each string input regardless which
+    way the user types the selections.
+    """
+
     return the_word
 
 
+def check_value(the_value, the_array):
+
+    """
+    This function used in conjuction with the add_data functions
+    above checks if the values entered are valid.
+    """
+
+    if the_value in the_array:
+        return True
+    else:
+        return False
 
 
 def add_listings():
-    # input_reference = "ABC"
-    # input_location = "def"
-    # input_bedrooms = 3
-    # input_cost = 300
-    # input_parking = "No"
-    # input_type = "House"
+
+    """
+    This function used in conjuction with the add_data functions
+    above checks if the values entered are valid.
+    """
+
     print("You entered: " + (input_reference) + ", " + (input_location) + ", " + str(input_bedrooms) + ", " + (input_parking) + ", " + str(input_cost) + ", " + (input_type))
     user_input = [input_reference, input_location, input_bedrooms, input_parking, input_cost, input_type]
-
-
-    # print("The code got this far, yay!")
-
-
 
     confirm = []
     global user_selections
@@ -219,7 +238,6 @@ def add_listings():
 
     if user_selections == ("Yes"):
         print("\nThe listing is being updated . . .\n")
-        # listings_data = SHEET.rentals("rentals")
         rentals.append_row(user_input)
         print("- - - Success! - - -\n")
         print("Returning to main menu . . .")
@@ -243,14 +261,6 @@ def delete_listings():
     when the user selects "3" from the main menu.
     """
 
-    """
-    Plan here is to create code to load the info from the
-    top row and allow the user to delete data.
-    1. The user will need to see the full row of data for each listing.
-    2. The user will need to be able to select the row of data to delete.
-    3. The user will need to confirm to delete the selected data.
-    """
-
     print("Please make sure the info is correct\n")
 
     ref_num = input("Please enter the reference number: ")
@@ -272,13 +282,13 @@ def delete_listings():
             delete_listings()
 
 
-"""
-This code runs all the functions in the program.
-"""
 def main():
+
+    """
+    This code runs all the functions in the program.
+    """
+
     load_start()
     validate_selection()
 
 main()
-
-# add_listings()
