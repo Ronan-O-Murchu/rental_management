fbb892a9a8836b6c72b9ec028b8d7b9584eca483
Ronan-O-Murchu <ronan.murphy.design@gmail.com>
2021-09-15 13:35:32 +0000

    DOCS: update all print statements and add listing function along with testing to see if it works

--- run.py
+++ run.py
@@ -23,14 +23,7 @@ def load_start():
     """
 
     print("\nWelcome to the online property management app for the")
-    print("Auckland, Wellington and Christchurch regions.\n")
-
-    print("Please make a choice from the options below.\n")
-
-    print("Type 1, 2 or 3 and press enter to make the selection.\n")
-    print("1: View most recent listings")
-    print("2: Add a property to the database")
-    print("3: Remove a property from the database\n")
+    print("Auckland, Wellington and Christchurch regions.")
 
 
 def validate_selection():
@@ -39,7 +32,13 @@ def validate_selection():
     This code validates the users selection on the main menu
     """
 
-    user_choice = int(input("Make a selection between 1 and 3: "))
+    print("\nPlease make a choice from the options below.\n")
+
+    print("1: View most recent listings")
+    print("2: Add a property to the database")
+    print("3: Remove a property from the database\n")
+
+    user_choice = int(input("Make a selection between 1 and 3:\n"))
     if user_choice == 1:
         display_latest_listings()
     elif user_choice == 2:
@@ -47,24 +46,23 @@ def validate_selection():
     elif user_choice == 3:
         delete_listings()
     else:
-        print("That is not a valid selection,")
+        print("\nThat is not a valid selection,")
         print("please enter a selection between 1 and 3.")
 
 
 def display_latest_listings():
 
     """
-    This code will display the 20 most recent listings when the user makes
+    This code will display the 10 most recent listings when the user makes
     the selection "1" from the main menu.
     """
 
     rows_data = []
     global show_listings
     show_listings = rentals.get_all_values()
-    for rows_data in range(1, 21):
+    for rows_data in range(2, 11):
         row = rentals.row_values(rows_data)
         print(*row)
-    load_start()
     validate_selection()
 
 
@@ -82,13 +80,15 @@ def add_reference():
     until the data is uploaded to the database.
     """
 
+    print("\nPlease enter the 3 character reference code for the property.")
+    print("Example: 123, ABC, A01 and then press enter\n")
     user_reference = []
     global input_reference
-    input_reference = input("\nPlease enter the reference number: ")
+    input_reference = input("Enter the data here:\n")
 
     if len(input_reference) != 3:
-        print("That is not a valid input, please try again.")
-        print("Example: 1234\n")
+        print("\nThat is not a valid input, please try again.")
+        print("Example: 123, ABC, A01\n")
         add_reference()
     else:
         print("That is a valid entry, thank you\n")
@@ -101,16 +101,18 @@ def add_location():
     This function requests the property location from the user.
     """
 
+    print("Please enter the lcoation either")
+    print("Auckland, Wellington or Christchurch\n")
     user_location = []
     global input_location
-    input_location = input("Please enter the location: ")
+    input_location = input("Enter the data here:\n")
     location = ["Auckland", "Wellington", "Christchurch"]
 
     if check_value(input_location, location):
         print("That is a valid entry, thank you\n")
         add_bedrooms()
     else:
-        print("That is not a valid entry, try again")
+        print("\nThat is not a valid input, please try again.")
         print("Please enter Auckland, Wellington or Christchurh\n")
         add_location()
 
@@ -122,16 +124,17 @@ def add_bedrooms():
     The users input is also converted into an INT.
     """
 
+    print("Please enter the ammount of bedrooms in the property.")
+    print("The bedrooms range from 1-5\n")
     user_bedrooms = []
     global input_bedrooms
-    input_bedrooms = input("Please enter the amount of bedrooms: ")
-    bedrooms = ["1", "2", "3", "4", "5"]
+    input_bedrooms = int(input("Enter the data here:\n"))
 
-    if check_value(input_bedrooms, bedrooms):
+    if 1 <= input_bedrooms <= 5:
         print("That is a valid entry, thank you\n")
         add_parking()
     else:
-        print("That is not a valid input, please try again.")
+        print("\nThat is not a valid input, please try again.")
         print("Example: 1, 2, 3, 4 or 5\n")
         add_bedrooms()
 
@@ -142,16 +145,18 @@ def add_parking():
     This function requests if the property has parking available from the user.
     """
 
+    print("Please enter if parking is available at the property")
+    print("by typing Yes or No and then pressing enter.\n")
     user_parking = []
     global input_parking
-    input_parking = input("Please enter if parking is available: ")
+    input_parking = input("Enter the data here:\n")
     parking = ["Yes", "No"]
 
     if check_value(input_parking, parking):
         print("That is a valid entry, thank you\n")
         add_cost()
     else:
-        print("That is not a valid input, please try again.")
+        print("\nThat is not a valid input, please try again.")
         print("Example: Yes or No\n")
         add_parking()
 
@@ -162,17 +167,17 @@ def add_cost():
     This function requests the property cost from the user.
     """
 
+    print("Please enter the rental price for the property.\n")
     user_cost = []
     global input_cost
-    input_cost = input("Please enter the cost: ")
-    cost = ["300", "350", "400", "450", "500", "550", "600"]
+    input_cost = int(input("Enter the data here:\n"))
 
-    if check_value(input_cost, cost):
+    if 300 <= input_cost <= 1000:
         print("That is a valid entry, thank you\n")
         add_type()
     else:
-        print("That is not a valid input, please try again.")
-        print("Prices are in between $300 - $600\n")
+        print("\nThat is not a valid input, please try again.")
+        print("Prices are in between $300 - $1000\n")
         add_cost()
 
 
@@ -182,18 +187,43 @@ def add_type():
     This function requests the property type from the user.
     """
 
+    print("Please enter the type of property this is.")
+    print("Example: Please type House, Apartment or Studio and press enter\n")
     user_type = []
     global input_type
-    input_type = makeCap(input("Please enter the type of rental: "))
+    input_type = input("Enter the data here:\n")
     type = ["House", "Apartment", "Studio"]
 
     if check_value(input_type, type):
         print("That is a valid entry, thank you\n")
-        add_listings()
+        add_availability()
     else:
-        print("That is not a valid input, please try again.")
+        print("\nThat is not a valid input, please try again.")
         print("Example: House, Apartment or Studio\n")
         add_type()
+
+
+def add_availability():
+
+    """
+    This function requests data from the user to determine
+    if this property is occupied or not.
+    """
+
+    print("Please enter if this property is currently available.")
+    print("Example: Please type Available or Occupied and press enter\n")
+    user_type = []
+    global input_availability
+    input_availability = makeCap(input("Enter the data here:\n"))
+    available = ["Available", "Occupied"]
+
+    if check_value(input_availability, available):
+        print("That is a valid entry, thank you\n")
+        add_listings()
+    else:
+        print("\nThat is not a valid input, please try again.")
+        print("Example: Please type Available or Occupied and press enter\n")
+        add_availability()
 
 
 def makeCap(the_word):
@@ -228,12 +258,12 @@ def add_listings():
     above checks if the values entered are valid.
     """
 
-    print("You entered: " + (input_reference) + ", " + (input_location) + ", " + str(input_bedrooms) + ", " + (input_parking) + ", " + str(input_cost) + ", " + (input_type))
-    user_input = [input_reference, input_location, input_bedrooms, input_parking, input_cost, input_type]
+    print("You entered: " + str(input_reference) + ", " + str(input_location) + ", " + str(input_bedrooms) + ", " + str(input_parking) + ", " + str(input_cost) + ", " + str(input_type) + ", " + str(input_availability))
+    user_input = [input_reference, input_location, str(input_bedrooms), input_parking, str(input_cost), input_type, input_availability]
 
     confirm = []
     global user_selections
-    user_selections = input("\nDo you want to update the database with this new data: ")
+    user_selections = input("\nDo you want to update the database with this new data:\n")
     user_confirmation = ["Yes", "No"]
 
     if user_selections == ("Yes"):
@@ -241,12 +271,10 @@ def add_listings():
         rentals.append_row(user_input)
         print("- - - Success! - - -\n")
         print("Returning to main menu . . .")
-        load_start()
         validate_selection()
     elif user_selections == ("No"):
         print("\nThe listing is has not been updated\n")
         print("Returning to main menu . . .")
-        load_start()
         validate_selection()
     else:
         print("\nThat is not a valid input, please try again.")
@@ -261,23 +289,26 @@ def delete_listings():
     when the user selects "3" from the main menu.
     """
 
+    print("Please enter the 3 character reference code for the property to delete.")
+    print("Example: 123, ABC, A01 and then press enter\n")
     print("Please make sure the info is correct\n")
 
-    ref_num = input("Please enter the reference number: ")
+    ref_num = input("Enter the data here: ")
 
     if len(ref_num) != 3:
-        print("That is not a valid input, please try again.")
-        print("Example: 123")
+        rentals.pop_row(ref_num)
+        print("\nThat is not a valid input, please try again.")
+        print("Example: 123, ABC, A01")
         delete_listings()
     else:
         print(*{ref_num})
-        confirm_delete = input("Are you sure you want to delete this data: ")
+        confirm_delete = input("Are you sure you want to delete this data:\n")
         confirm = ["Yes"]
 
-        if confirm_delete in confirm:
+        if check_value(confirm_delete, confirm):
             print("That entry has now been deleted\n")
         else:
-            print("That is not a valid input, please try again.")
+            print("\nThat is not a valid input, please try again.")
             print("Example: Yes or No\n")
             delete_listings()
 
@@ -292,3 +323,5 @@ def main():
     validate_selection()
 
 main()
+
+# add_type()
